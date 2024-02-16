2c2125698ae480157969538bbc74acb057432b97
Michael <mabramo11@yahoo.com>
2024-02-16 07:12:07 -0500

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { ConnectWallet } from '@thirdweb-dev/react'
+import Banner from './components/Banner'
+import UnclaimedNFTs from './components/UnclaimedNFTs'
+
+type Props = {}
+
+function App({ }: Props) {
+  return (
+    <div className="container mx-auto">
+      {/* connect button  */}
+      <div className="py-5 flex justify-between">
+        <ConnectWallet btnTitle="Connect" />
+      </div>
+      {/* banner */}
+      <div className="mb-14">
+        <Banner />
+      </div>
+      {/* unclaimed nfts component */}
+      <UnclaimedNFTs />
+    </div>
+  )
+}
+
+export default App
---
+++ src/config.ts
@@ -0,0 +1,2 @@
+export const contractAddress = "0x6c4CEc9B16084aEd19954752dE5d00e64E1Bc311";
+export const activeChain = "mumbai";
